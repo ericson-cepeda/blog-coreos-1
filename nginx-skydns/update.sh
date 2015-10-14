ede8bdfa6cba7c2d9547883e674a784bcfbd923c
CoreOS Admin <core@ip-10-2-5-41.us-west-2.compute.internal>
2015-10-14 22:50:16 +0000

    Simplified endpoints.

--- nginx-skydns/update.sh
+++ nginx-skydns/update.sh
@@ -1,4 +1,6 @@
+docker build -t twnel/nginx-sky:dev .
+
 docker rm -f nginx
-docker build -t twnel/nginx-sky:latest .
-source /etc/environment; docker run -d --name nginx -p 80:80 -p 443:433 -e DOMAIN=twnel.com -e REGION=beta -v $HOME/apn_cert/certs/:/etc/nginx/certs/ -e TRUSTED=1 -e HOST_IP=${COREOS_PRIVATE_IPV4} twnel/nginx-sky
+source /etc/environment; docker run -d --name nginx -p 80:80 -p 443:433 -e DOMAIN=twnel.com -e REGION=api -e CLUSTER=beta -e HOST_IP=${COREOS_PRIVATE_IPV4} twnel/nginx-sky:dev
+
 docker logs nginx
